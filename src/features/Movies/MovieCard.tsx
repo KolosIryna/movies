4aaa320f964e24f59f34601570bd05298dd63395
KolosIryna <127253667+KolosIryna@users.noreply.github.com>
2024-04-12 16:43:12 +0200

    2

--- src/features/Movies/MovieCard.tsx
+++ src/features/Movies/MovieCard.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
-
-import "./Movies.css";
+import styles from "./MovieCard.module.scss";
 
 interface MovieCardProps {
   id: number;
@@ -23,10 +22,19 @@ interface MovieCardProps {
 
 export function MovieCard({ id, title, overview, popularity }: MovieCardProps) {
   return (
-    <div className="Movies-card">
-      <Link to={`/movies/${id}`}>{title}</Link>
-      <div className="Movies-card-overview">{overview}</div>
-      <div className="Movies-card-popularity">{popularity}</div>
+    <div className={styles.card}>
+      <img
+        className={styles.thumbnail}
+        src="/movie-thumb.png"
+        alt="Movie thumbnail"
+      />
+      <div className={styles.content}>
+        <div>
+          <Link to={`/movies/${id}`}>{title}</Link>
+        </div>
+        <span className={styles.overview}>{overview}</span>
+        <div className={styles.popularity}>{popularity}</div>
+      </div>
     </div>
   );
 }
